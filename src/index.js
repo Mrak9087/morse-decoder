642c91204bbfde4f9b9eb0d4299cb68bb6a9211b
Mrak9087 <59642198+Mrak9087@users.noreply.github.com>
2020-09-15 19:36:26 +0400

    feat: implements function decode()

--- src/index.js
+++ src/index.js
@@ -39,6 +39,13 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+    return expr.match(/.{10}/g).map((chr) =>{
+        if (chr.charAt(0) === '*') {
+            return ' ';
+        }
+        return MORSE_TABLE[chr.replace(/00/g,'').replace(/11/g,'-').replace(/10/g,'.')];
+    }
+    ).join('');
 }
 
 module.exports = {
